64bc4330132fb5d1fcbcad0ca888f47832dc7f78
Riccardo Meggiolaro <riccardomeggiolaro.info@protonmail.com>
2025-09-01 12:33:59 +0200

    tare can be nullable and add min 100 and max 99999

--- src/api/card/card.entity.ts
+++ src/api/card/card.entity.ts
@@ -20,12 +20,12 @@ export class CardORM extends BaseEntity {
     @Column({type: "varchar", length: 10, nullable: true})
     plate: string;
 
+    @Column({type: "int", default: null, nullable: true})
+    tare: number;
+
     @Column({type: "int", nullable: true})
     @JoinColumn({ name: 'materialId' })
     materialId: number;
-
-    @Column({type: "int", default: 0, nullable: false})
-    tare: number;
 
     @Column({type: "varchar", length: 50, nullable: true})
     note: string;
